398d5caf257cc9f41cd9d33c16c64047be5d8fdb
Sama Manalai <manalais@berea.edu>
2020-02-08 17:10:39 -0500

    Added tweet and user files

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -0,0 +1,22 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class User {
+
+    public String name;
+    public String screenName;
+    public String profileImageUrl;
+
+
+    public static User fromJson(JSONObject jsonObject) throws JSONException {
+
+        User User = new User();
+        User.name = jsonObject.getString("name");
+        User.screenName = jsonObject.getString("screen_name");
+        User.profileImageUrl = jsonObject.getString("profile_image_url_http");
+        return User;
+    }
+
+}
